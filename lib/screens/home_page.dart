f6b36b5e32c5a8c133d645a61cd97a415b2d3a90
nazeerahmad <nazeerahmad876@gmail.com>
2021-03-23 23:41:09 +0530

    day11 complete

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,21 +1,29 @@
 import "package:flutter/material.dart";
+import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
-
+import 'package:flutter_catalog/widgets/item_widget.dart';
+// day 11 studied contraints and widget tree
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(30, (index) => CatalogModel.items[0]);
     int days = 30;
     String dev = "Development";
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Catalog App'),
-      ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to $days days of flutter $dev"),
+        appBar: AppBar(
+          title: Text('Catalog App'),
         ),
-      ),
-      drawer: MyDrawer(),
-    );
+        drawer: MyDrawer(),
+        body: Padding(
+          padding: EdgeInsets.all(16),
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ItemWidget(
+                item: dummyList[index],
+              );
+            },
+            itemCount: dummyList.length,
+          ),
+        ));
   }
 }
